0c0f25a2fd2728fe732931031dd7943f5fefea16
Rafael Lacerda <lacerdabusatta@gmail.com>
2021-08-22 17:25:17 -0300

    Criação do modal para feedback visual do registro de despesa ao usuário; Refactoring do modal, alterando os dados de acordo com sucesso ou erro, evitando dois modais no index.html;

--- js/app.js
+++ js/app.js
@@ -1,19 +1,63 @@
 class Despesa {
-    constructor(ano, dia, mes, tipo, descricao, valor) {
+    constructor(ano, mes, dia, tipo, descricao, valor) {
         this.ano = ano
-        this.dia = dia
         this.mes = mes
+        this.dia = dia
         this.tipo = tipo
         this.descricao = descricao
         this.valor = valor
     }
 
     verificaDados() {
-        console.log("Verificação de dados ativa.")
+        for(let i in this) {
+            if(this[i] == undefined || this[i] == '' || this[i] == null) {
+                //dados inválidos
+                return false
+            }
+        }
+        //dados válidos
+        return true
     }
 
-    cadastroDespesa() {
-        console.log("Cadastro de despesa feito com sucesso!")
+    cadastrarDespesa() {
+        let ano = document.querySelector("#ano")
+        let mes = document.querySelector("#mes")
+        let dia = document.querySelector("#dia")
+        let tipo = document.querySelector("#tipo")
+        let descricao = document.querySelector("#descricao")
+        let valor = document.querySelector("#valor")
+
+        let modalText = document.querySelector("#modal-text")
+        let modalBtn = document.querySelector("#btn-back")
+        let modalTitle = document.querySelector("#caixaDeDialogoDeRegistro")
+        let modalBody = document.querySelector("#body")
+
+        let novaDespesa = new Despesa(
+            ano.value, 
+            mes.value, 
+            dia.value, 
+            tipo.value, 
+            descricao.value, 
+            valor.value
+        )
+
+        if(novaDespesa.verificaDados()) {
+            //dados são gravados
+            bd.gravar(novaDespesa)
+            $('#modalRegistraDespesa').modal('show')
+            modalText.className = "modal-header text-success"
+            modalBtn.className = "btn btn-success"
+            modalTitle.innerHTML = "Registro inserido com sucesso"
+            modalBody.innerHTML = "Despesa foi cadastrada com sucesso!"
+        }
+        else {
+            //dados não serão armazenados
+            $('#modalRegistraDespesa').modal('show')
+            modalText.className = "modal-header text-danger"
+            modalBtn.className = "btn btn-danger"
+            modalTitle.innerHTML = "Erro na inclusão de registro"
+            modalBody.innerHTML = "Erro, verifique se todos os dados do formulário foram preenchidos corretamente e tente novamente!"
+        }
     }
 
     consultaDespesas() {
@@ -25,14 +69,31 @@ class Despesa {
     }
 }
 
-function verificaDados(ano, mes, dia, tipo, descricao, valor){
-    if((ano || mes || dia ||tipo || descricao || valor) === "") {
-        return 1
+let despesa = new Despesa()
+
+class BD {
+    constructor() {
+        let id = localStorage.getItem('id')
+
+        if(id === null) {
+            localStorage.setItem('id', 0)
+        }
     }
-    return 0
+    getProximoId() {
+        let proximoId = localStorage.getItem('id')
+        return (parseInt(proximoId) + 1)
+    }
+
+    gravar(d) {
+        let id = this.getProximoId()
+        localStorage.setItem(id, JSON.stringify(d))
+        localStorage.setItem('id', id)
+    }
 }
 
-function cadastrarDespesa() {
+let bd = new BD()
+
+/*function cadastrarDespesa() {
     let ano = document.querySelector("#ano")
     let mes = document.querySelector("#mes")
     let dia = document.querySelector("#dia")
@@ -40,7 +101,7 @@ function cadastrarDespesa() {
     let descricao = document.querySelector("#descricao")
     let valor = document.querySelector("#valor")
 
-    if(verificaDados(ano.value, mes.value, dia.value, tipo.value, descricao.value, valor.value) == 1) {
+    if(desp.verificaDados(ano.value, mes.value, dia.value, tipo.value, descricao.value, valor.value) == 1) {
         alert("Erro, verifique os dados do formulário e tente novamente")
     }
     else {
@@ -55,10 +116,13 @@ function cadastrarDespesa() {
 
         console.log(despesa)
     
-        gravar(despesa)
+        bd.gravar(despesa)
     }
-}
+}*/
 
-function gravar(d) {
-    localStorage.setItem('despesa', JSON.stringify(d))
-}
+/*function verificaDados(ano, mes, dia, tipo, descricao, valor){
+    if((ano || mes || dia ||tipo || descricao || valor) === "") {
+        return 1
+    }
+    return 0
+} */
