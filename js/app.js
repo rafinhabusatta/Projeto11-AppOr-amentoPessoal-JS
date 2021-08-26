17a4a111a1ea0384d05b49cb4932d3d4604839e2
Rafael Lacerda <lacerdabusatta@gmail.com>
2021-08-26 14:46:20 -0300

    Implementação de nova função consulta; Implementação de função no banco de dados para armazenamento das despesas em array;

--- js/app.js
+++ js/app.js
@@ -20,6 +20,7 @@ class Despesa {
     }
 
     cadastrarDespesa() {
+        let form = document.querySelector("#form")
         let ano = document.querySelector("#ano")
         let mes = document.querySelector("#mes")
         let dia = document.querySelector("#dia")
@@ -49,6 +50,7 @@ class Despesa {
             modalBtn.className = "btn btn-success"
             modalTitle.innerHTML = "Registro inserido com sucesso"
             modalBody.innerHTML = "Despesa foi cadastrada com sucesso!"
+            form.reset()
         }
         else {
             //dados não serão armazenados
@@ -81,52 +83,135 @@ class BD {
         localStorage.setItem(id, JSON.stringify(d))
         localStorage.setItem('id', id)
     }
-    consultaDespesas() {
-        console.log("Consulta de despesas ativa.")
+
+    recuperarTodosRegistros() {
+        let id = localStorage.getItem('id')
+        let despesas = Array()
+
+        for(let i = 1; i <= id; i++) {
+
+            let despesa = JSON.parse(localStorage.getItem(i)) //leitura do localStorage da despesa e conversão de JSON para Objeto
+            if(despesa === null) {
+                continue // pula para a próxima iteração do laço
+            }
+            despesas.push(despesa) //inclui a despesa no array despesas
+        } 
+        return despesas  
     }
 
     removerDespesa() {
         console.log("Remoção de despesa feita com sucesso!")
+    }
+
+    pesquisar(despesa) {
+        let filtro = despesa
+        let despesas = bd.recuperarTodosRegistros()
+        let despesasFiltradas = Array()
+
+        despesas.forEach(function(d) {   
+            if(filtro.tipo != null) {
+                if(d.tipo == filtro.tipo) {
+                    despesasFiltradas.push(d)
+                }
+            }
+            if(filtro.descricao != null) {
+                if(d.descricao == filtro.descricao) {
+                    despesasFiltradas.push(d)
+                }
+            }
+            if(filtro.valor != null) {
+                if(d.valor == filtro.valor) {
+                    despesasFiltradas.push(d)
+                }
+            }
+        })
+        return despesasFiltradas
     }
 }
 
 let bd = new BD()
 
 function carregaListaDespesas() {
+    let despesas = bd.recuperarTodosRegistros()
     let tableBody = document.querySelector("#table-body") //corpo da tabela
-    let trNova = document.createElement("tr") //criação da table row
-    tableBody.appendChild(trNova) //adiciona a tr dentro do corpo da tabela
+    //let i = 0
 
-    do {
-        let i = 1
-        let despesa = JSON.parse(localStorage.getItem(i)) //leitura do localStorage da despesa e conversão de JSON para Objeto
+    despesas.forEach(function(d) {
+        let trNova = tableBody.insertRow()
+
+        //ajuste de tipo
+        switch(d.tipo) {
+            case 'alimentacao': d.tipo = 'Alimentação'
+                break
+            case 'educacao': d.tipo = 'Educação'
+                break
+            case 'lazer': d.tipo = 'Lazer'
+                break
+            case 'saude': d.tipo = 'Saúde'
+                break
+            case 'transporte': d.tipo = 'Transporte'
+                break
+        }
+        trNova.insertCell(0).innerHTML = `${d.dia}/${d.mes}/${d.ano}`
+        trNova.insertCell(1).innerHTML = d.tipo
+        trNova.insertCell(2).innerHTML = d.descricao
+        trNova.insertCell(3).innerHTML = d.valor
+    })
+
+    /*while(i <= despesas.length) {
+        
+        //criação da table row
+        let trNova = document.createElement("tr")
+        tableBody.appendChild(trNova) //adiciona a tr dentro do corpo da tabela
 
         //criação de table data para Data
         let tdData = document.createElement("td")
         trNova.appendChild(tdData)
-        let data = document.createTextNode(`${despesa.dia}/${despesa.mes}/${despesa.ano}`)
+        let data = document.createTextNode(`${despesas[i].dia}/${despesas[i].mes}/${despesas[i].ano}`)
         tdData.appendChild(data) //adiciona o nó de texto à nova div criada
 
         //criação de table data para Tipo
         let tdTipo = document.createElement("td")
         trNova.appendChild(tdTipo)
-        let tipo = document.createTextNode(`${despesa.tipo}`)
+        let tipo = document.createTextNode(`${despesas[i].tipo}`)
         tdTipo.appendChild(tipo) //adiciona o nó de texto à nova div criada
 
         //criação de table data para Descrição
         let tdDescricao= document.createElement("td")
         trNova.appendChild(tdDescricao)
-        let descricao = document.createTextNode(`${despesa.descricao}`)
+        let descricao = document.createTextNode(`${despesas[i].descricao}`)
         tdDescricao.appendChild(descricao) //adiciona o nó de texto à nova div criada
         
         //criação de table data para Valor
         let tdValor = document.createElement("td")
         trNova.appendChild(tdValor)
-        let valor = document.createTextNode(`${despesa.valor}`)
+        let valor = document.createTextNode(`${despesas[i].valor}`)
         tdValor.appendChild(valor) //adiciona o nó de texto à nova div criada
 
         i++
-    } while(despesa)
+    }*/
+}
+
+function pesquisaDespesas() {
+    let ano = document.querySelector("#ano")
+    let mes = document.querySelector("#mes")
+    let dia = document.querySelector("#dia")
+    let tipo = document.querySelector("#tipo")
+    let descricao = document.querySelector("#descricao")
+    let valor = document.querySelector("#valor")
+
+    let despesa = new Despesa(
+        ano.value, 
+        mes.value, 
+        dia.value, 
+        tipo.value, 
+        descricao.value, 
+        valor.value
+    )
+
+    let despesas = bd.pesquisar(despesa)
+
+    console.log(despesas)
 }
 
 /*function cadastrarDespesa() {
@@ -149,8 +234,6 @@ function carregaListaDespesas() {
             descricao.value, 
             valor.value
         )
-
-        console.log(despesa)
     
         bd.gravar(despesa)
     }
